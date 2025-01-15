e2db3c660d3ec13e9824b45e4976ed450c516515
Neal Patel <nealp3034@gmail.com>
2025-01-15 13:11:12 -0500

    Fix error message at beginning of program run
    Tab everything once after if statement to get rid of app error message

--- app.py
+++ app.py
@@ -18,27 +18,26 @@ if uploaded_file is not None:
 
     'PDF succesfully uploaded!'
 #pdf.pages gives a list of all pages
-st.write(f"Number of pages {len(pdf.pages)}")
+    st.write(f"Number of pages {len(pdf.pages)}")
 
 
-doc = Document()
+    doc = Document()
 
-#for each page in the pdf
-for page_num in range(len(pdf.pages)):
-    page = pdf.pages[page_num] #single page
-    text = page.extract_text() #extracted text
-    doc.add_paragraph(text) #extracted text gets added to doc
+    #for each page in the pdf
+    for page_num in range(len(pdf.pages)):
+        page = pdf.pages[page_num] #single page
+        text = page.extract_text() #extracted text
+        doc.add_paragraph(text) #extracted text gets added to doc
 
+    #repositioning new doc file
+    word_io =io.BytesIO()
+    doc.save(word_io)
+    word_io.seek(0)
+#create new download button with metadata
+    st.download_button(
+        label= "Download Converted File",
+        data= word_io,
+        file_name= "Converted.docx",
+        mime = "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
 
-word_io =io.BytesIO()
-doc.save(word_io)
-
-word_io.seek(0)
-
-st.download_button(
-    label= "Download Converted File",
-    data= word_io,
-    file_name= "Converted.docx",
-    mime = "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
-
-)
+    )
