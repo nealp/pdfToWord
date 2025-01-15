f720d771f98f334d1fdeb63bfb1cdc23bdaac8ef
Neal Patel <nealp3034@gmail.com>
2025-01-15 12:36:13 -0500

    Setup PDF reader with message
    Initialize PDFreader object with a message to show it works

--- app.py
+++ app.py
@@ -8,3 +8,11 @@ st.title('PDF to Word Converter')
 st.write("Upload your PDF here:")
 
 uploaded_file= st.file_uploader(" ", type = ["pdf"])
+
+#if there is a file uploaded:
+if uploaded_file is not None: 
+    pdf = PdfReader(uploaded_file)
+
+    'PDF succesfully uploaded!'
+
+    
